cee8ed8fc49fc21a10ace14fc1848209f60b292d
unknown <johnwhitakeraustin@gmail.com>
2020-09-21 10:49:10 -0700

    The site's coming along

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
 
 app=Flask(__name__)
-app.config["MONGO_URI"]="mongodb://localhost:27017/NAMEOFDBHERE"
+app.config["MONGO_URI"]="mongodb://localhost:27017/mars_db"
 mongo=PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars_intel.find_one()
-    return render_template("index.html",mars_intel=mars_intel)
+    mars = mongo.db.mars.find_one()
+    return render_template(
+        "index.html",
+        mars=mars,
+    )
 
 @app.route("/scrape")
 def scraper():
-    mars_intel=mongo.db.mars_intel
+    mars=mongo.db.mars
     mars_update = scrape_mars.scrape()
-    mars_intel.update({},mars_update,upsert=True)
+    mars.update({},mars_update,upsert=True)
     return redirect(url_for('index'),code=302)
 
 if __name__=="__main__":
-    ap.run(debug=True)
+    app.run(debug=True)
